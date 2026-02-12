ca6ce4d0c71d648a24340532ad401d914e0c7d5c
Akrit Chauhan <156348636+Akritchauhan@users.noreply.github.com>
2026-02-12 14:52:55 +0530

    Initial commit

--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
     return HttpResponse("hello")
+def about(request):
+    return HttpResponse("about")
--- dj1/urls.py
+++ dj1/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('blog/',home,name='home'),
+    # path('blog/about/',about,name='about'),
+    path('',include('blog.urls')),
 ]

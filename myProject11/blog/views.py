ee9ae1828461a7f3074d9e35bf43f1b57904406e
Akrit Chauhan <156348636+Akritchauhan@users.noreply.github.com>
2026-02-20 11:47:09 +0530

    learned using boostrap in Django

---
+++ myProject11/blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'base.html')
+def blog(request):
+    student = [
+        {'name': 'Alice', 'age': 20},
+        {'name': 'Bob', 'age': 22},
+    ]
+    return render(request,'blog.html', {'students': student})

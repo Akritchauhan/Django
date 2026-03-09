1a5d44b5d1b07b7b3248be5f97744a5fba1bf3da
Akrit Chauhan <156348636+Akritchauhan@users.noreply.github.com>
2026-03-09 22:56:31 +0530

    learned Modelforms CRUD

--- myproject19/student/urls.py
+++ myproject19/student/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.student_create,name='student_create'),
+    path('students/',views.student_list,name='student_list'),
+    path('students/<int:pk>/',views.student_detail,name='student_detail'),
+    path('students/<int:pk>/edit/',views.student_update,name='student_update'),
+    path('students/<int:pk>/delete/',views.student_delete,name='student_delete'),
 ]

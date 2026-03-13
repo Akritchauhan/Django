652ed839a58f8ef2a380d6b2fdd51aa6ab356da8
Akrit Chauhan <156348636+Akritchauhan@users.noreply.github.com>
2026-03-13 22:30:45 +0530

    learned about pagination

---
+++ myproject24/blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Post
+from django.core.paginator import Paginator
+
+def post_list(request):
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 1)  # Show 1 post per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/post_list.html', {'page_obj': page_obj})

752a47cc5cc9b9ccd0576cb53b98cc13d52808cc
Akrit Chauhan <156348636+Akritchauhan@users.noreply.github.com>
2026-03-20 14:54:10 +0530

    leraned about perview cache

---
+++ myproject36/blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import UserList
+from django.views.decorators.cache import cache_page
+
+@cache_page(30)
+
+def users_list(request):
+    print("Fetching user list from database")
+
+    users=UserList.objects.all()
+    return render(request,'users.html',{'users':users})

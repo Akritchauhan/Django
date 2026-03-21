3189642dfd558ee020f163223a1585d65ad097a4
Akrit Chauhan <156348636+Akritchauhan@users.noreply.github.com>
2026-03-21 17:01:24 +0530

    Learned about rest api (GET ,POST ,PATCH ,PUT , DELETE)

--- djangorestapi/apiproject02/api/urls.py
+++ djangorestapi/apiproject02/api/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('students/',views.student_list,name='student_list')
+    path('students/',views.student_list,name='student_list'),
+    path('add/',views.add_studentlist,name='add_studentlist'),
+    path('update/<int:pk>',views.update_student,name='update_student'),
+    path('delete/<int:pk>',views.delete_student,name='delete_student')
 ]
